3ea1b690787feaf137401fb8bea5a56fa84c4a9d
Chaitanya D <chaitu583@gmail.com>
2023-05-23 09:00:49 +0530

    Update1

--- src/a/File1.java
+++ src/a/File1.java
@@ -4,6 +4,6 @@ public class File1 {
 	
 	public static void main(String[] args) {
 		System.out.println("In Branch A");
-		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("BBBBBBBBBBBBBBBBBBBBBBBB2 :Creating Merge Conflict");
 	}
 }
