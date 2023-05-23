6666a605392338ac916c5256c6d3eb4ce9dfb368
Chaitanya D <chaitu583@gmail.com>
2023-05-23 08:15:31 +0530

    Making Changes in A1 Branch

--- src/a/File1.java
+++ src/a/File1.java
@@ -4,5 +4,6 @@ public class File1 {
 	
 	public static void main(String[] args) {
 		System.out.println("In Branch A");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
 	}
 }
--- src/basicConcept/StringPractice.java
+++ src/basicConcept/StringPractice.java
@@ -4,5 +4,7 @@ public class StringPractice {
 	
 	public static void main(String[] args) {
 		System.out.println("I am in Main Method");
+		System.out.println("Hi");
+		System.out.println("Making Changes for Merge Conflict in Branch A1");
 	}
 }
